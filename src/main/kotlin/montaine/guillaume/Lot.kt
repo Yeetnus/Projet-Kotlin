32f741574421702a9e63018e1225394605f2fd6a
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2024-12-09 14:59:35 +0100

    remove getters

--- src/main/kotlin/montaine/guillaume/Lot.kt
+++ src/main/kotlin/montaine/guillaume/Lot.kt
@@ -4,16 +4,8 @@ import montaine.guillaume.Manga
 
 data class Lot(val quantite : Int, val manga : Manga) {
 
-    fun getQuantite(): Int {
-        return quantite
-    }
-
-    fun getManga(): Manga {
-        return manga
-    }
-
     fun getPrix(): Double {
-        return this.getQuantite() * this.getManga().getPrix()
+        return this.quantite * this.manga.Prix
     }
 
 }
--- src/main/kotlin/montaine/guillaume/Manga.kt
+++ src/main/kotlin/montaine/guillaume/Manga.kt
@@ -2,12 +2,4 @@ package montaine.guillaume
 
 data class Manga(val Titre: String, val Prix: Double) {
 
-    fun getTitre(): String {
-        return Titre
-    }
-
-    fun getPrix(): Double {
-        return Prix
-    }
-
 }
