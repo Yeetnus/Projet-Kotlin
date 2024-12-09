f4deb817dbaa31c35cb1eff240770366903324b5
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2024-12-09 15:51:05 +0100

    fix taxes

--- src/main/kotlin/montaine/guillaume/Pays.kt
+++ src/main/kotlin/montaine/guillaume/Pays.kt
@@ -2,10 +2,10 @@ package org.example.montaine.guillaume.montaine.guillaume
 
 enum class Pays(val taxes: Double) {
 
-    FRANCE(20.0),
-    ESPAGNE(18.2),
-    ALLEMAGNE(15.6),
-    ROYAUME_UNI(22.8),
-    BELGIQUE(17.0);
+    FRANCE(0.2),
+    ESPAGNE(0.182),
+    ALLEMAGNE(0.156),
+    ROYAUME_UNI(0.228),
+    BELGIQUE(0.17);
 
 }
