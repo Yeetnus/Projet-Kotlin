e7e3d6b337f7878012e1aca12ff4c7581b3a09ee
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2024-12-09 16:38:32 +0100

    handle prix manga negative

--- src/main/kotlin/montaine/guillaume/Manga.kt
+++ src/main/kotlin/montaine/guillaume/Manga.kt
@@ -2,4 +2,10 @@ package montaine.guillaume
 
 data class Manga(val Titre: String, val Prix: Double) {
 
+    init {
+        if (Prix < 0) {
+            throw IllegalArgumentException("Le prix ne peut pas être négatif")
+        }
+    }
+
 }
