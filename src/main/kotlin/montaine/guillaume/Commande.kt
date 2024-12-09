98342866cc3eeceebdfa8249c264f6bc6eef8f26
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2024-12-09 15:40:09 +0100

    class commandes functions

--- src/main/kotlin/montaine/guillaume/Commande.kt
+++ src/main/kotlin/montaine/guillaume/Commande.kt
@@ -9,4 +9,36 @@ data class Commande(val Boutique: Boutique, val Lots: List<Lot>) {
         return this.Lots.sumOf { it.Quantite }
     }
 
+    fun getMontantHT(): Double {
+        return this.Lots.sumOf { it.getPrix()*it.Quantite }
+    }
+
+    fun getRemise(): Double {
+        if (this.getMontantHT() < 150) {
+            return 0.0
+        } else if (this.getMontantHT() < 200) {
+            return this.getMontantHT()*0.02
+        } else if (this.getMontantHT() < 300) {
+            return this.getMontantHT()*0.03
+        } else if (this.getMontantHT() < 500) {
+            return this.getMontantHT()*0.05
+        } else if (this.getMontantHT() < 1000) {
+            return this.getMontantHT()*0.07
+        } else {
+            return this.getMontantHT()*0.1
+        }
+    }
+
+    fun getMontantRemise(): Double {
+        return this.getMontantHT() - this.getRemise()
+    }
+
+    fun getTaxes(): Double {
+        return this.getMontantRemise()*this.Boutique.Pays.taxes
+    }
+
+    fun getMontantTTC(): Double {
+        return this.getMontantRemise() + this.getTaxes()
+    }
+
 }
